40ee4e2ce7098ed08649d8f635b7da5ba53e6d41
saragalko <sara.anya.galko33@gmail.com>
2024-06-21 00:55:57 +0300

    Something from diplom

---
+++ diplom/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    return render(request, 'about.html', {'title': 'О клубе Виденье'})
